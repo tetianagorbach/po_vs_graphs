bacc99ba114975ae2108d783f28e34dca0049fc2
tetianagorbach <tanya.gorbach@gmail.com>
2024-05-27 09:48:57 +0200

    corrected example 4

--- Example4_m_bias.R
+++ Example4_m_bias.R
@@ -1,5 +1,5 @@
 # Author: Juha Karvanen/ code Tetiana Gorbach
-# Date: 2024-05-17
+# Date: 2024-05-27
 # This code presents an example of M-bias.
 
 # Load required library
@@ -14,43 +14,45 @@ set.seed(13959077)
 # Set the sample size
 n <- 1000000
 
-u1 <- rbinom(n, size = 1, prob = 0.5)
-u2 <- rbinom(n, size = 1, prob = 0.5)
-
-
-# c  <-  (u1+u2) %% 2
-c1 <- rbinom(n, size = 1, prob = plogis(u1 + u2))
+u1 <- rbinom(n, size = 1, prob = 0.6)
+u2 <- rbinom(n, size = 1, prob = 0.4)
+c  <-  (u1+u2) %% 2
 
 a <- rbinom(n, size = 1, prob = 0.1 + 0.8*u1)
 y1 <-  numeric(n)
 y0 <-  numeric(n)
-y1[u2 == 1] <-  rbinom(sum(u2 == 1), 
-                                size = 1, 
-                                prob = 0.9)
+y1[u2 == 1] <-  rbinom(sum(u2 == 1),
+                       size = 1,
+                       prob = 0.9)
 y1[u2 == 0] <-  rbinom(sum(u2 == 0), 
-                                size = 1, 
-                                prob = 0.1)
+                       size = 1, 
+                       prob = 0.1)
 y0[u2 == 1] <-  rbinom(sum( u2 == 1), size = 1, 
-                                prob = 0.9)
-
+                       prob = 0.1)
 y0[u2 == 0] <-  rbinom(sum(u2 == 0), size = 1, 
-                                prob = 0.1)
+                       prob = 0.9)
 mean(y1) - mean(y0)
 
 # Generate observed outcome
 y <- ifelse(a == 1, y1, y0)
 
-ci.test(y0, as.numeric(a), as.numeric(c1)) 
+ci.test(y0, as.numeric(a), as.numeric(c)) # unconfoundness is not fulfilled
+ci.test(y1, as.numeric(a), as.numeric(c)) 
+ci.test(as.numeric(u1), as.numeric(c), as.numeric(u2)) # c depends on u1 and u2
+ci.test(as.numeric(u2), as.numeric(c), as.numeric(u1))
+
+ci.test(as.numeric(y1), as.numeric(u2)) # y(1) and y(0) depend on u2
+ci.test(as.numeric(y0), as.numeric(u2))
 
 # EY(1)
 mean(y1) 
 # EY(1) using the adjustment for C
-mean(y[a==1 & c1==1])* mean(c1) + mean(y[a==1 & c1==0])*(1-mean(c1))
+mean(y[a==1 & c==1])* mean(c) + mean(y[a==1 & c==0])*(1-mean(c))
 
 # EY(0)
 mean(y0) 
 # EY(0) using the adjustment for C
-mean(y[a==0 & c1==1])* mean(c1) + mean(y[a==0 & c1==0])*(1-mean(c1))
+mean(y[a==0 & c==1])* mean(c) + mean(y[a==0 & c==0])*(1-mean(c))
 
 
 
@@ -58,9 +60,9 @@ mean(y[a==0 & c1==1])* mean(c1) + mean(y[a==0 & c1==0])*(1-mean(c1))
 # ATE
 mean(y1) - mean(y0)
 # ATE estimated through the adjustment
-lm(y ~ a + c1)
+lm(y ~ a + c)
 
-mean(y[a==1 & c1==1])* mean(c1) + mean(y[a==1 & c1==0])*(1-mean(c1)) -
-mean(y[a==0 & c1==1])* mean(c1) - mean(y[a==0 & c1==0])*(1-mean(c1))
+mean(y[a==1 & c==1])* mean(c) + mean(y[a==1 & c==0])*(1-mean(c)) -
+mean(y[a==0 & c==1])* mean(c) - mean(y[a==0 & c==0])*(1-mean(c))
 
 
